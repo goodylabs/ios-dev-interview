037f4d4adc68cd87d8aac03231f98e36eb211d18
Marcin Pałosz <palosz.marcin@gmail.com>
2022-03-30 14:29:37 +0200

    Add required fields to Character model

--- ios-dev-interview/Networking/Services/Character/CharacterModel.swift
+++ ios-dev-interview/Networking/Services/Character/CharacterModel.swift
@@ -13,10 +13,17 @@ struct CharacterResponse: Codable {
     var results: [Character]?
 }
 
+enum CharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case unknown
+}
+
 struct Character: Codable {
     var id: Int?
     var name: String?
     var status: String?
     var species: String?
     var image: String?
+    var gender: CharacterGender?
 }
