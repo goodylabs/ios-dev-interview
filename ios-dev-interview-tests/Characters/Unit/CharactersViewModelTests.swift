bcbd5810ee82df02d02a164b90329f3375b6bfb3
Marcin Pałosz <palosz.marcin@gmail.com>
2022-03-31 21:55:08 +0200

    Add a couple of tests for VMs

--- ios-dev-interview-tests/Characters/Unit/CharactersViewModelTests.swift
+++ ios-dev-interview-tests/Characters/Unit/CharactersViewModelTests.swift
@@ -9,12 +9,45 @@
 import XCTest
 import RxSwift
 import RxTest
+
 @testable import IOS_DEV_INTERVIEW
 
+struct CharacterServiceMock: CharacterService {
+    
+    func getCharacters(page: Int) -> Observable<CharacterResponse> { characters }
+
+    func getCharacter(id: Int) -> Observable<Character> { character }
+    
+    let characters: Observable<CharacterResponse>
+    let character: Observable<Character>
+    
+    init(characters: Observable<CharacterResponse>? = nil, character: Observable<Character>? = nil) {
+        self.characters = characters ?? Observable.just(CharacterResponse(info: .init(pages: 0)))
+        self.character = character ?? Observable.just(Character())
+    }
+
+}
+
 class CharactersViewModelTests: XCTestCase {
-
-    func test() {
-
+    func testFetchCharacterShouldUpdateCharacter() {
+        // Given
+        let character = Observable.just(Character(id: 5, name: "Marcin"))
+        let service = CharacterServiceMock(character: character)
+        let viewModel = CharacterDetailsViewModel(service: service, id: 5)
+        // When
+        viewModel.fetchCharacter()
+        // Then
+        //I would check if character id and name are equal to viewModel.character id and name by using XCTAssert
     }
     
+    func testFetchCharactersListShouldUpdateCharacters() {
+        // Given
+        let characters = Observable.just(CharacterResponse(info: .init(pages: 0), results: [Character(id: 5, name: "Marcin"), Character(id: 7, name: "Jacek")]))
+        let service = CharacterServiceMock(characters: characters)
+        let viewModel = CharactersListViewModel(service: service)
+        // When
+        viewModel.fetchCharacters(page: 0)
+        //Then
+        //I would check if both characters id and name are equal to viewModel.characters id and name by using XCTAssert
+    }
 }
